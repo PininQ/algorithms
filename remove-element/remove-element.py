45eb153f98268aea477dfff1180af13f5c3fd2a8
qinbin52qiul <im.qinbin@gmail.com>
2018-10-08 10:11:30 +0800

    移除元素

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -25,12 +25,18 @@ class Solution:
             i += 1
         return length
 
+    # 执行用时：48 ms
+    def removeElement3(self, nums, val):
+        while val in nums:
+            nums.remove(val)
+        return len(nums)
+
 
 if __name__ == '__main__':
     arr1, val1 = [4, 4, 0, 1, 0, 2], 0
     # print(Solution().removeElement(arr1, val1))
     arr2, val2 = [0, 1, 2, 2, 3, 0, 4, 2], 2
-    n = Solution().removeElement2(arr1, val1)
+    n = Solution().removeElement3(arr1, val1)
     print(n)
     for i in range(n):
         print(arr1[i], end=', ')
