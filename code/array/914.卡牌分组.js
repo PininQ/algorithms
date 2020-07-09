05c8c3f55cecacdba5132aadb7bef1765624665a
PininQ <im.qinbin@gmail.com>
2020-07-09 23:49:55 +0800

    JavaScript 914.卡牌分组.js

--- code/array/914.卡牌分组.js
+++ code/array/914.卡牌分组.js
@@ -45,7 +45,30 @@ var hasGroupsSizeX2 = function (deck) {
   return g >= 2
 };
 
+var hasGroupsSizeX3 = function (deck) {
+  // 将卡牌按值排序保证相同的卡牌是挨着的
+  let str = deck.sort((a, b) => a - b).join('')
+  // 分组（单张或者多张）
+  let group = str.match(/(\d)\1+|\d/g)
+  // 求两个数的最大公约数
+  let gcd = (a, b) => b === 0 ? a : gcd(b, a % b)
+  // 思路：只要所有的分组具有最大公约数大于1 就满足条件
+  while(group.length > 1) {
+    let a = group.shift().length
+    let b = group.shift().length
+    let v = gcd(a, b)
+    if (v < 2)
+      return false
+    else
+      // 将前两个分组的最大公约数推进数组与下一个分组再次验证是否有最大公约数
+      group.unshift('0'.repeat(v))
+  }
+  // 边界 ['11'] 只有一个分组
+  return group.length ? group[0].length > 1 : false
+};
+
 export {
   hasGroupsSizeX,
   hasGroupsSizeX2,
+  hasGroupsSizeX3
 }
