07294817b2f4e9d2459e30036872d403c55c6d59
PininQ <im.qinbin@gmail.com>
2020-07-15 23:58:30 +0800

    JavaScript 冒泡排序

---
+++ code/sort/冒泡排序.js
@@ -0,0 +1,16 @@
+var bubbleSort = function (arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+export {
+  bubbleSort
+}
---
+++ test/sort/冒泡排序.test.js
@@ -0,0 +1,9 @@
+import bubbleSort from '../../code/sort/冒泡排序'
+
+test('bubbleSort：1', () => {
+  expect(bubbleSort([2, 3, 1])).toEqual([1, 2, 3])
+})
+
+test('bubbleSort：2', () => {
+  expect(bubbleSort([2, 17, 9, 11])).toEqual([2, 9, 9, 11, 17])
+})
