8058af4b2793c5692c9d1e796403831b60aeb7d4
PininQ <im.qinbin@gmail.com>
2020-07-17 23:18:21 +0800

    JavaScript 922.按奇偶排序数组 II

--- code/sort/922.按奇偶排序数组 II.js
+++ code/sort/922.按奇偶排序数组 II.js
@@ -38,22 +38,53 @@ var sortArrayByParityII2 = function (A) {
   return res
 }
 
+/**
+ * 当偶数位出来奇数时，一定有奇数位出现偶数
+ * 以偶数位来遍历，0、2、4、...，如果遇到奇数值，则使用奇数位来找偶数，1、3、5...，
+ * 如果找到了，则交换值，然后进入下一轮偶数遍历
+ * @param {Array} A
+ */
 var sortArrayByParityII3 = function (A) {
   let odd = 1
 
-  for (let i = 0; i < A.length; i += 2) {
-    if (A[i] % 2 === 1) {
+  // 1. 如果为偶数，则进入下一次循环，检查下一个偶数下标的值是否为偶数
+  // 2. 如果非偶数，就用 odd 找到一个奇数下标的值为偶数来交换
+  for (let even = 0; even < A.length; even += 2) {
+    if (A[even] % 2 === 1) {
+      // 偶数位出现不为偶数的值，说明奇数位肯定能找到偶数
+      // 2.1 跳过奇数，找到一个奇数位为偶数值的下标
       while (A[odd] % 2 === 1) {
         odd += 2
       }
-      ;[A[i], A[odd]] = [A[odd], A[i]]
+      // 2.2 奇数位找到偶数了，将偶数位的奇数和奇数位的偶数进行交换
+      ;[A[even], A[odd]] = [A[odd], A[even]]
+      odd += 2
     }
   }
   return A
 }
 
+var sortArrayByParityII4 = function (A) {
+  let oddIndex = 1
+  let evenIndex = 0
+  let res = []
+
+  for (let i = 0, len = A.length; i < len; i++) {
+    if (A[i] % 2 === 0) {
+      res[evenIndex] = A[i]
+      evenIndex += 2
+    } else {
+      res[oddIndex] = A[i]
+      oddIndex += 2
+    }
+  }
+
+  return res
+}
+
 export {
   sortArrayByParityII,
   sortArrayByParityII2,
-  sortArrayByParityII3
+  sortArrayByParityII3,
+  sortArrayByParityII4
 }
--- test/sort/922.按奇偶排序数组 II.test.js
+++ test/sort/922.按奇偶排序数组 II.test.js
@@ -1,4 +1,4 @@
-import { sortArrayByParityII, sortArrayByParityII2, sortArrayByParityII3 } from '../../code/sort/922.按奇偶排序数组 II'
+import { sortArrayByParityII, sortArrayByParityII2, sortArrayByParityII3, sortArrayByParityII4 } from '../../code/sort/922.按奇偶排序数组 II'
 
 test('sortArrayByParityII', () => {
   expect(sortArrayByParityII([4, 2, 5, 7, 1, 6])).toEqual([4, 5, 2, 7, 6, 1])
@@ -10,4 +10,8 @@ test('sortArrayByParityII2', () => {
 
 test('sortArrayByParityII3', () => {
   expect(sortArrayByParityII3([4, 2, 5, 7, 1, 6])).toEqual([4, 5, 2, 7, 6, 1])
+})
+
+test('sortArrayByParityII4', () => {
+  expect(sortArrayByParityII4([4, 2, 5, 7, 1, 6])).toEqual([4, 5, 2, 7, 6, 1])
 })
