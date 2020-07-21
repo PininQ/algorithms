f66d319624bc0b2a0356acf492396993e3bb1b05
PininQ <im.qinbin@gmail.com>
2020-07-21 23:51:19 +0800

    JavaScript 93. 复原IP地址

--- code/recur/93. 复原IP地址.js
+++ code/recur/93. 复原IP地址.js
@@ -28,6 +28,35 @@ var restoreIpAddresses = function (str) {
   return res
 }
 
+var restoreIpAddresses2 = function (str) {
+  let len = str.length
+  let res = []
+  if (len < 4 || len > 12) return res
+
+  for (let i = 1; i < 4; i++) {
+    for (let j = i + 1; j < i + 4; j++) {
+      if (j >= len) break
+      for (let k = j + 1; k < j + 4; k++) {
+        if (k >= len) break
+        let a = str.slice(0, i)
+        let b = str.slice(i, j)
+        let c = str.slice(j, k)
+        let d = str.slice(k)
+        if (d.length > 3) continue
+        if (test(a, b, c, d)) res.push(`${a}.${b}.${c}.${d}`)
+      }
+    }
+  }
+
+  return res
+}
+
+var test = function (a, b, c, d) {
+  let t = x => x === '0' || (x <= 255 && x.indexOf('0') !== 0)
+  return t(a) && t(b) && t(c) && t(d)
+}
+
 export {
-  restoreIpAddresses
+  restoreIpAddresses,
+  restoreIpAddresses2
 }
