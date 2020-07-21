e26c6f929c7b16414daca50e180cdc01dca34abe
PininQ <im.qinbin@gmail.com>
2020-07-22 00:11:07 +0800

    JavaScript 93. 复原IP地址

--- code/recur/93. 复原IP地址.js
+++ code/recur/93. 复原IP地址.js
@@ -51,12 +51,47 @@ var restoreIpAddresses2 = function (str) {
   return res
 }
 
+// ip 为 0~255 之间
 var test = function (a, b, c, d) {
   let t = x => x === '0' || (x <= 255 && x.indexOf('0') !== 0)
   return t(a) && t(b) && t(c) && t(d)
 }
 
+var restoreIpAddresses3 = function (str) {
+  let len = str.length
+  let res = []
+  if (len < 4 || len > 12) return []
+  fn(str, [], res)
+  return res
+}
+
+var fn = function (remain, temp, result) {
+  // 递归结束条件：当判断到最后一段时，如果合法直接加入到结果集
+  if (temp.length === 3) {
+    regular(remain) && result.push([...temp, remain].join('.'))
+    return
+  }
+  // 递归体：每一段长度可以为1、2、3，所以每次都有三种可能
+  for (let i = 1; i < 4; i++) {
+    regular(remain.substr(0, i)) && fn(remain.substr(i), [...temp, remain.substr(0, i)], result)
+  }
+}
+
+/**
+ * 判断每一段是否合法，是否合法需要满足以下条件：
+ * 大于等于0；
+ * 小于等于255
+ * 如果是一位可以为0，如果超过一位，不能以0开头
+ * @param {string} s
+ * @returns {boolean}
+ */
+var regular = function (s) {
+  if (!s.length) return false
+  return +s >= 0 && +s <= 255 && (s.length > 1 ? !!+s[0] : true)
+}
+
 export {
   restoreIpAddresses,
-  restoreIpAddresses2
+  restoreIpAddresses2,
+  restoreIpAddresses3
 }
